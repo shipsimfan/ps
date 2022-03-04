b894bb189f22fe03a84e460fc8a13353f99d061a
Lance Hart <shipsimfan@hotmail.com>
2022-03-04 14:51:02 -0500

    First commit

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <los/session.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    sid_t sid = get_session_id();
+
+    isize num_processes = get_session_processes(sid, NULL, 0);
+    if (num_processes < 0) {
+        fprintf(stderr, "Failed to get processes: %s\n",
+                strerror(num_processes));
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t *processes = (pid_t *)malloc(sizeof(pid_t) * num_processes);
+    num_processes = get_session_processes(sid, processes, num_processes);
+    if (num_processes < 0) {
+        fprintf(stderr, "Failed to get processes: %s\n",
+                strerror(num_processes));
+        exit(EXIT_FAILURE);
+    }
+
+    process_t info;
+    info.working_directory = (char *)malloc(sizeof(char) * 1024);
+    info.working_directory_len = 1024;
+    info.name = (char *)malloc(sizeof(char) * 256);
+    info.name_len = 256;
+
+    for (isize i = 0; i < num_processes; i++) {
+        isize ret = get_process_info(sid, processes[i], &info);
+        if (ret < 0)
+            continue;
+
+        printf("%ld %s (%lds)\n", processes[i], info.name, info.time / 1000);
+        printf("    (Threads: %ld) (Files: %ld) (Directories: %ld)\n",
+               info.num_threads, info.num_files, info.num_directories);
+        printf("    %s\n", info.working_directory);
+    }
+
+    free(processes);
+    free(info.name);
+    free(info.working_directory);
+}
